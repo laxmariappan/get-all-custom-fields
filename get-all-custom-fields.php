37e1206d909ee51713b5ae7c072a4453a0680cc1
Lax Mariappan <lax.mariappan@webdevstudios.com>
2023-04-01 04:35:05 +0530

    add plugin file and index.php

---
+++ get-all-custom-fields.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Get All Custom Fields
+ * Plugin URI:        https://github.com/laxmariappan/get-all-custom-fields
+ * Description:       A WordPress plugin to list all custom fields in a site.
+ * Version:           1.0.0
+ * Requires at least: 6.1
+ * Requires PHP:      7.4
+ * Author:            Lax Mariappan
+ * Author URI:        https://laxmariappan.com/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Update URI:        FALSE
+ * Text Domain:       get-all-custom-fields
+ * Domain Path:       /languages
+ *
+ * @package get-all-custom-fields
+ */
